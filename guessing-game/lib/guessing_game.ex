de42575b481b1dbbf43b94e2d77717e412f5e52e
Kevin Du <kkxandeer@gmail.com>
2022-05-15 15:33:12 +0800

    learned default arguments and abs

---
+++ guessing-game/lib/guessing_game.ex
@@ -0,0 +1,8 @@
+defmodule GuessingGame do
+  def compare(_, guess \\ :no_guess)
+  def compare(_, guess) when not is_integer(guess), do: "Make a guess"
+  def compare(secret_number, secret_number), do: "Correct"
+  def compare(secret_number, guess) when abs(secret_number - guess) == 1, do: "So close"
+  def compare(secret_number, guess) when guess > secret_number, do: "Too high"
+  def compare(secret_number, guess) when guess < secret_number, do: "Too low"
+end
